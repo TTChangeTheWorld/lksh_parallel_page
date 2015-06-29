eb366786f4f4100bc1d74102bfcc6b2258b751e7
Artem Tabolin <artemtab@yandex.ru>
2015-06-29 18:58:04 +0300

    First version of dictionary (without tags)

--- config.py
+++ config.py
@@ -22,28 +22,24 @@ def get_config():
                 ("Условия", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/problems.pdf"),
                 ("Вход", "https://ejudge.lksh.ru/cgi-bin/new-client?contest_id=21501&locale_id=1"),
                 ("Таблица результатов", "https://ejudge.lksh.ru/ejudge/standings/021501.html"),
-                ("Словарь", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/Словарь.pdf")
             ]),
         Day("День 2: Продолжение",
             [
                 ("Условия", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/problems.pdf"),
                 ("Вход", "https://ejudge.lksh.ru/cgi-bin/new-client?contest_id=21501&locale_id=1"),
                 ("Таблица результатов", "https://ejudge.lksh.ru/ejudge/standings/021501.html"),
-                ("Словарь", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/Словарь.pdf")
             ]),
         Day("День 3: Уже почти конец",
             [
                 ("Условия", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/problems.pdf"),
                 ("Вход", "https://ejudge.lksh.ru/cgi-bin/new-client?contest_id=21501&locale_id=1"),
                 ("Таблица результатов", "https://ejudge.lksh.ru/ejudge/standings/021501.html"),
-                ("Словарь", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/Словарь.pdf")
             ]),
         Day("День 4: Зачет",
             [
                 ("Условия", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/problems.pdf"),
                 ("Вход", "https://ejudge.lksh.ru/cgi-bin/new-client?contest_id=21501&locale_id=1"),
                 ("Таблица результатов", "https://ejudge.lksh.ru/ejudge/standings/021501.html"),
-                ("Словарь", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/Словарь.pdf"),
                 ("Еще что-то странное", "https://ejudge.lksh.ru/archive/2014/08/Cpy/01/Словарь.pdf")
             ])
     ])
